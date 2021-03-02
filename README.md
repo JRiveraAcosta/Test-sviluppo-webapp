620096d150a6bb2053fe1ad602082b4736187bf5
JRiveraAcosta <73483276+JRiveraAcosta@users.noreply.github.com>
2021-03-02 19:07:10 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+DEV CHALLENGE
+===============================
+
+### Risorse
+- php 7.4.11
+- mysql 10.1.37-MariaDB
+
+### mysql
+
+- creazione e inserimento dati sono presenti nel file bdwebapp.sql nella cartella doc.
---
+++ bdwebapp.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE `orders` (
+  `idorders` int(11) NOT NULL,
+  `time_creation` datetime DEFAULT CURRENT_TIMESTAMP,
+  `user_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+INSERT INTO `orders` (`idorders`, `time_creation`, `user_name`) VALUES
+(1, '2021-03-02 18:01:46', 'Jimmy'),
+(2, '2021-03-02 18:03:47', 'Fabio');
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`idorders`);
+ALTER TABLE `orders`
+  MODIFY `idorders` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
